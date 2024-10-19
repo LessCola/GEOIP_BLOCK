b821311048d5b77b2d310ac27f754830e3afc789
LessCola <32204399+LessCola@users.noreply.github.com>
2024-10-19 21:50:58 +0800

    Update main.sh

--- main.sh
+++ main.sh
@@ -110,9 +110,9 @@ check_status() {
     else
         echo "Creating IPv4 chain $CHAIN_NAME and adding to INPUT."
         iptables -N $CHAIN_NAME
+        iptables -A INPUT -j $CHAIN_NAME
         iptables -I $CHAIN_NAME 1 -p tcp -s 0.0.0.0/0 --dport 22 -j ACCEPT
         iptables -A $CHAIN_NAME -j DROP
-        iptables -A INPUT -j $CHAIN_NAME
     fi
 
     # 检查并创建 IPv6 链
@@ -121,9 +121,9 @@ check_status() {
     else
         echo "Creating IPv6 chain $CHAIN_NAME and adding to INPUT."
         ip6tables -N $CHAIN_NAME
+        ip6tables -A INPUT -j $CHAIN_NAME
         ip6tables -I $CHAIN_NAME 1 -p tcp -s ::/0 --dport 22 -j ACCEPT
         ip6tables -A $CHAIN_NAME -j DROP
-        ip6tables -A INPUT -j $CHAIN_NAME
     fi
 }
 
